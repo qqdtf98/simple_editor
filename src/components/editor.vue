dda45afb29df62a2e118f1b5aa0e04abf458498a
qqdtf98 <qqdf98@gmail.com>
2020-01-04 11:58:25 +0900

    initial commit

---
+++ src/components/editor.vue
@@ -0,0 +1,210 @@
+<template>
+    <div id="dashboard">
+        <div @click="onmouseClick" @mousemove="onmouseMove">
+            <div class="dashboard-wrapper">
+                <div class="text-wrapper">
+                    <div class="dashboard-text">
+                        Dashboard
+                    </div>
+                    <button class="generate">Generate Report</button>
+                </div>
+                <div class="component-wrapper">
+                    <div class="earning-box">
+                        <div class="earningm-wrapper">
+                            <span class="earningm">
+                                EARNINGS(MONTHLY)
+                            </span>
+                            <button class="button1">a</button>
+                        </div>
+                        <div class="earninga-wrapper">
+                            <span class="earninga">
+                                EARNINGS(ANNUAL)
+                            </span>
+                            <button class="button2">a</button>
+                        </div>
+                    </div>
+                    <div class="task-box">
+                        <div class="tasks-wrapper">
+                            <span class="tasks">
+                                TASKS
+                            </span>
+                            <button class="button3">a</button>
+                        </div>
+                        <div class="pending-wrapper">
+                            <span class="pending">
+                                PENDING REQUESTS
+                            </span>
+                            <button class="button4">a</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="selector-box">
+
+                <div class="tagname"></div>
+
+                <div class="compo-border"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {compo: null, borderstyle: null}
+        },
+        methods: {
+            onmouseMove(e) {
+                if (this.compo === null) {
+                    this.compo = e
+                        .target
+                        .getBoundingClientRect()
+                    let tag = document.querySelector('.tagname')
+                    tag.textContent = e.target.tagName
+                    tag.style.left = this.compo.left + 'px'
+                    tag.style.top = this.compo.top + 'px'
+                    let bord = document.querySelector('.compo-border')
+                    bord.style.border = "1px solid #3e8ce4"
+                    bord.style.left = this.compo.left + 'px'
+                    bord.style.top = this.compo.top + 'px'
+                    bord.style.width = this.compo.width + 'px'
+                    bord.style.height = this.compo.height + 'px'
+                } else {
+                    if (this.compo != e.target) {
+                        this.compo = e
+                            .target
+                            .getBoundingClientRect()
+                        let tag = document.querySelector('.tagname')
+                        tag.textContent = e.target.tagName
+                        tag.style.left = this.compo.left + 'px'
+                        tag.style.top = this.compo.top - tag
+                            .getBoundingClientRect()
+                            .height + 'px'
+                        let bord = document.querySelector('.compo-border')
+                        bord.style.border = "1px solid #3e8ce4"
+                        bord.style.left = this.compo.left + 'px'
+                        bord.style.top = this.compo.top + 'px'
+                        bord.style.width = this.compo.width + 'px'
+                        bord.style.height = this.compo.height + 'px'
+                    }
+                }
+            },
+            onmouseLeave(e) {
+                e.target.style.border = "none"
+            },
+            onmouseClick(e) {
+                console.log(e.target)
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .earninga-wrapper {
+        border: 1px solid #1cc88a;
+        width: 20rem;
+        display: flex;
+        flex-direction: row;
+        padding: 0.5rem;
+        border-radius: 0.4rem;
+
+        box-shadow: 1px 0.5px 0.5px #c0c0c0;
+    }
+    .earninga {
+        color: #1cc88a;
+    }
+    #dashboard {
+        display: flex;
+        align-items: center;
+        width: 100%;
+    }
+    .earningm-wrapper {
+        border: 1px solid #d95353;
+        box-shadow: 1px 0.5px 0.5px #c0c0c0;
+        width: 20rem;
+        display: flex;
+        flex-direction: row;
+        padding: 0.5rem;
+        border-radius: 0.4rem;
+        margin-right: 1rem;
+        align-items: center;
+    }
+    .earningm {
+        color: #d95353;
+    }
+    .tasks-wrapper {
+        border: 1px solid #36b9cc;
+
+        box-shadow: 1px 0.5px 0.5px #c0c0c0;
+        width: 20rem;
+        display: flex;
+        flex-direction: row;
+        padding: 0.5rem;
+        border-radius: 0.4rem;
+        margin-right: 1rem;
+    }
+    .tasks {
+        color: #36b9cc;
+    }
+    .pending-wrapper {
+        border: 1px solid #f6c23e;
+        width: 20rem;
+        display: flex;
+        flex-direction: row;
+        padding: 0.5rem;
+        box-shadow: 1px 0.5px 0.5px #c0c0c0;
+        border-radius: 0.4rem;
+    }
+    .pending {
+        color: #f6c23e;
+    }
+    .text-wrapper {
+        display: flex;
+        flex-direction: row;
+        margin: 0.5rem;
+    }
+    .task-box {
+        display: flex;
+        flex-direction: row;
+    }
+    .component-wrapper {
+        padding: 0.5rem;
+        /* border: 1px solid black; */
+    }
+    .generate {
+        background-color: #8b8bcc;
+        border-radius: 0.4rem;
+    }
+    .earning-box {
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 1rem;
+    }
+    .button1,
+    .button2,
+    .button3,
+    .button4 {
+        background-color: #afa7a7;
+        margin-left: 1rem;
+        border: none;
+    }
+    .compo-border {
+        position: fixed;
+        z-index: -1;
+    }
+    .tagname {
+        color: white;
+        padding: 0.2rem;
+        position: fixed;
+        z-index: 10;
+        background-color: #3e8ce4;
+        border-radius: 0.4rem;
+        font-size: 0.8rem;
+    }
+    .selector-box {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
