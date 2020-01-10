c929ff2b8105b4aa9dc3127b58e9233f197f9045
sungmin <qqdtf98@gmail.com>
2020-01-10 17:54:23 +0900

    chore: add components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-import store from './store';
+// import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
