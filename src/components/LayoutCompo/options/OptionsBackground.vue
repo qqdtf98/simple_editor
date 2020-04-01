9fd1a4442122fd5cbf96daaef760f93786c3ee49
qqdtf98 <qqdtf98@gmail.com>
2020-04-01 17:44:12 +0900

    Create new layout design and separate component

--- src/components/LayoutCompo/options/OptionsBackground.vue
+++ src/components/LayoutCompo/options/OptionsBackground.vue
@@ -1,1 +1,83 @@
+<template>
+  <div id="options-background">
+    <div class="bg-color-wrapper">
+      <div class="bg-color-text">Background Color</div>
+      <div class="bg-color-list">
+        <button class="color-none" />
+        <button
+          v-for="i in 14"
+          id="color-button"
+          :key="`color-picker-${i}`"
+          class="color-choose"
+          :class="'color' + i"
+        />
+        <button class="chrome-picker" />
+      </div>
+    </div>
+    <div class="bg-img-wrapper">
+      <div class="bg-img-text">Background Image</div>
+      <button class="bg-img-input">Pick</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
 @import '../../../assets/scss/globalstyle';
+#options-background {
+  display: flex;
+  margin-top: 0.3rem;
+  margin-bottom: 0.3rem;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  .bg-color-wrapper {
+    width: 80%;
+    .bg-color-text {
+      margin-top: 0.4rem;
+      margin-bottom: 0.4rem;
+      color: #868686;
+    }
+    .bg-color-list {
+      margin-top: 0.4rem;
+      margin-bottom: 0.4rem;
+      align-items: center;
+      justify-content: center;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 10px;
+      .color-choose,
+      .chrome-picker,
+      .color-none {
+        width: 100%;
+        height: 2rem;
+        display: inline-block;
+        border-radius: 0.3rem;
+        border: none;
+        transition: box-shadow 200ms ease;
+
+        &:hover {
+          box-shadow: 0 0 0 0.2rem #40b3ff;
+        }
+        &::before {
+          content: '';
+          display: block;
+          padding-top: 100%;
+        }
+      }
+    }
+  }
+  .bg-img-wrapper {
+    width: 80%;
+    display: flex;
+    flex-direction: row;
+    .bg-img-text {
+      margin-top: 0.4rem;
+      margin-bottom: 0.4rem;
+      color: #868686;
+    }
+    .bg-img-input {
+    }
+  }
+}
+</style>
---
+++ src/components/LayoutCompo/options/OptionsFilter.vue
@@ -0,0 +1,160 @@
+<template>
+  <div id="options-filter">
+    <div v-for="(filter, i) in filters" :key="i" class="filter-wrapper">
+      <div class="filter-text">{{ filter.name }}</div>
+      <range-slider
+        class="filter-slider"
+        min="filter.min"
+        max="filter.max"
+        step="filter.step"
+        name="filter.name"
+        submitSorce.style="filter.name"
+        @input="filter.onInput"
+        v-model="filter.vModel"
+      />
+      <input class="filter-input" />
+    </div>
+  </div>
+</template>
+
+<script>
+import RangeSlider from 'vue-range-slider'
+
+export default {
+  components: { RangeSlider },
+  data() {
+    return {
+      filters: [
+        {
+          name: 'Opacity',
+          min: 0,
+          max: 1,
+          step: 0.01,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Blur',
+          min: 0,
+          max: 100,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Brightness',
+          min: 0,
+          max: 100,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Constrast',
+          min: 0,
+          max: 200,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Grayscale',
+          min: 0,
+          max: 100,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Hue',
+          min: 0,
+          max: 360,
+          step: 3,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Invert',
+          min: 0,
+          max: 100,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Saturate',
+          min: 0,
+          max: 200,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        },
+        {
+          name: 'Sepia',
+          min: 0,
+          max: 100,
+          step: 1,
+          vModel: '',
+          onInput: () => {}
+        }
+      ],
+      submitSource: {
+        payload: '',
+        style: '',
+        value: '',
+        change: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#options-filter {
+  display: flex;
+  margin-top: 0.3rem;
+  margin-bottom: 0.3rem;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  .filter-wrapper {
+    display: flex;
+    margin-top: 0.3rem;
+    margin-bottom: 0.3rem;
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+    .filter-text {
+      font-size: 0.9rem;
+      text-align: center;
+      width: 7rem;
+      margin-right: 0.5rem;
+      color: #868686;
+    }
+    .filter-slider {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: calc(100% - 6rem);
+      .range-slider-inner {
+        width: 100%;
+      }
+      .range-slider-inner {
+        width: 70%;
+      }
+      .range-slider-knob {
+        width: 13px !important;
+        height: 13px !important;
+      }
+    }
+    .filter-input {
+      background: none;
+      border: none;
+      border-bottom: 1px solid #768ea7;
+      width: 3.5rem;
+    }
+  }
+}
+</style>
